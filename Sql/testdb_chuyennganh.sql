e0d6f733ec2c90253186304c2a02fac69d942c99
minhan1410 <minhan14102001@gmail.com>
2022-05-11 23:39:31 +0700

    db

--- Sql/testdb_chuyennganh.sql
+++ Sql/testdb_chuyennganh.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `testdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `testdb`;
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: localhost    Database: testdb
 -- ------------------------------------------------------
--- Server version	8.0.28
+-- Server version	8.0.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -39,7 +39,7 @@ CREATE TABLE `chuyennganh` (
 
 LOCK TABLES `chuyennganh` WRITE;
 /*!40000 ALTER TABLE `chuyennganh` DISABLE KEYS */;
-INSERT INTO `chuyennganh` VALUES ('111','Du Lich'),('222','Kinh Te'),('333','Toan Tin');
+INSERT INTO `chuyennganh` VALUES ('111','Du lịch'),('222','Kinh tế'),('333','Toán tin');
 /*!40000 ALTER TABLE `chuyennganh` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-11 10:33:57
+-- Dump completed on 2022-05-11 23:39:12
--- Sql/testdb_user.sql
+++ Sql/testdb_user.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `testdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `testdb`;
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: localhost    Database: testdb
 -- ------------------------------------------------------
--- Server version	8.0.28
+-- Server version	8.0.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -33,7 +33,7 @@ CREATE TABLE `user` (
   `Diachi` varchar(100) DEFAULT NULL,
   `Email` varchar(45) DEFAULT NULL,
   `CMND` varchar(12) DEFAULT NULL,
-  `avatar` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `avatar` varchar(1000) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
   `ngay_sinh` date DEFAULT NULL,
   `dien_thoai` varchar(10) DEFAULT NULL,
   `gioi_tinh` varchar(4) DEFAULT NULL,
@@ -61,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-11 10:33:57
+-- Dump completed on 2022-05-11 23:39:12
